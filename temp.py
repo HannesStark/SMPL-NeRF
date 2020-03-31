4c63f54c6c1c7b98fd2755a93510706c35ded1a9
hannes-staerk <hannes.staerk@gmail.com>
2020-03-31 10:37:57 +0200

    add texture.jpg and mess around with textures in tmp.py

--- temp.py
+++ temp.py
@@ -3,8 +3,10 @@ import argparse
 
 import numpy as np
 import torch
-
+import cv2
 import smplx
+from pyrender.material import MetallicRoughnessMaterial #MetallicRoughnessMaterial
+
 
 
 
@@ -18,6 +20,7 @@ output = model(betas=betas, expression=expression,
                return_verts=True)
 vertices = output.vertices.detach().cpu().numpy().squeeze()
 joints = output.joints.detach().cpu().numpy().squeeze()
+joints[0]= [0.1,0.1,0.1]
 
 print('Vertices shape =', vertices.shape)
 print('Joints shape =', joints.shape)
@@ -25,22 +28,29 @@ print('Joints shape =', joints.shape)
 
 import pyrender
 import trimesh
-vertex_colors = np.ones([vertices.shape[0], 4]) * [0.3, 0.3, 0.3, 0.8]
+vertex_colors = np.ones([vertices.shape[0], 4]) * [0.7, 0.3, 0.3, 0.8]
 tri_mesh = trimesh.Trimesh(vertices, model.faces,
                            vertex_colors=vertex_colors)
 
+image = cv2.imread('texture.jpg')
+#sampler = pyrender.sampler.Sampler()
+#texture = pyrender.Texture(source=image,sampler=sampler, source_channels='RGB')
+#material = pyrender.material.MetallicRoughnessMaterial(baseColorTexture=texture, wireframe=True)
 mesh = pyrender.Mesh.from_trimesh(tri_mesh)
+
+print(mesh)
+
 
 scene = pyrender.Scene()
 scene.add(mesh)
 
 
 sm = trimesh.creation.uv_sphere(radius=0.005)
-sm.visual.vertex_colors = np.random.uniform(size=sm.vertices.shape)
+sm.visual.vertex_colors = [1.0, 0.0, 0.0]
 tfs = np.tile(np.eye(4), (len(joints), 1, 1))
 tfs[:, :3, 3] = joints
 
 joints_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
 scene.add(joints_pcl)
 
-pyrender.Viewer(scene, use_raymond_lighting=True)
+pyrender.Viewer(scene, show_mesh_axes=True)
