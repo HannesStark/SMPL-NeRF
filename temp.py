7964ccba00986671af98c3774653105a201f9a26
hannes-staerk <hannes.staerk@gmail.com>
2020-04-01 09:43:08 +0200

    try different texture and color trimesh directly

--- temp.py
+++ temp.py
@@ -1,56 +1,34 @@
 import os.path as osp
 import argparse
+import pickle
+from io import BytesIO
 
 import numpy as np
 import torch
 import cv2
+from PIL import Image
 import smplx
-from pyrender.material import MetallicRoughnessMaterial #MetallicRoughnessMaterial
+import pyrender
+import trimesh
 
-
-
-
-model = smplx.create("models", model_type='smplx')
-
-print(model.parameters())
+model = smplx.create("models/model.pkl", model_type='smpl')
 betas = torch.randn([1, 10], dtype=torch.float32)
 expression = torch.randn([1, 10], dtype=torch.float32)
-
 output = model(betas=betas, expression=expression,
                return_verts=True)
 vertices = output.vertices.detach().cpu().numpy().squeeze()
-joints = output.joints.detach().cpu().numpy().squeeze()
-joints[0]= [0.1,0.1,0.1]
-
-print('Vertices shape =', vertices.shape)
-print('Joints shape =', joints.shape)
 
 
-import pyrender
-import trimesh
-vertex_colors = np.ones([vertices.shape[0], 4]) * [0.7, 0.3, 0.3, 0.8]
-tri_mesh = trimesh.Trimesh(vertices, model.faces,
-                           vertex_colors=vertex_colors)
+with open('texture.jpg', 'rb') as file:
+    texture = Image.open(BytesIO(file.read()))
 
-image = cv2.imread('texture.jpg')
-#sampler = pyrender.sampler.Sampler()
-#texture = pyrender.Texture(source=image,sampler=sampler, source_channels='RGB')
-#material = pyrender.material.MetallicRoughnessMaterial(baseColorTexture=texture, wireframe=True)
-mesh = pyrender.Mesh.from_trimesh(tri_mesh)
+uv = np.load('uv_table.npy')
+print(uv)
+smpl_mesh = trimesh.Trimesh(vertices, model.faces, visual=trimesh.visual.TextureVisuals(uv=uv, image=texture), process=False)
 
-print(mesh)
-
+pymesh = pyrender.Mesh.from_trimesh(smpl_mesh)
 
 scene = pyrender.Scene()
-scene.add(mesh)
+scene.add(pymesh)
 
-
-sm = trimesh.creation.uv_sphere(radius=0.005)
-sm.visual.vertex_colors = [1.0, 0.0, 0.0]
-tfs = np.tile(np.eye(4), (len(joints), 1, 1))
-tfs[:, :3, 3] = joints
-
-joints_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
-scene.add(joints_pcl)
-
-pyrender.Viewer(scene, show_mesh_axes=True)
+pyrender.Viewer(scene)
