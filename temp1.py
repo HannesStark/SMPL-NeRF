62b77ecd6532aba0dc4b072547f183dc048804b2
HannesStaerk <hannes.staerk@gmail.com>
2020-05-18 09:42:19 +0200

    asdfa

--- temp1.py
+++ temp1.py
@@ -9,11 +9,7 @@ import trimesh
 from trimesh.ray.ray_triangle import RayMeshIntersector
 import numpy as np
 
-from datasets.smpl_nerf_dataset import SmplNerfDataset
-from datasets.transforms import CoarseSampling
-
-transform = transforms.Compose(
-        [NormalizeRGB(), CoarseSampling(2, 6, 64), ToTensor()])
-
-train_dir = 'data'
-dataset = SmplNerfDataset(train_dir, os.path.join(train_dir, 'transforms.pkl'), transform)
+a = np.ones(((5,6,3)))
+b = np.ones((5))
+b = np.repeat(b[np.newaxis,:], 343, axis=0)
+print(b)
