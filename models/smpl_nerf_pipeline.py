6077762c9ebcf975e06718ba098f9ee497de2dae
HannesStaerk <hannes.staerk@gmail.com>
2020-05-25 08:53:50 +0200

    add experiment with no encoding for the warp field input

--- models/smpl_nerf_pipeline.py
+++ models/smpl_nerf_pipeline.py
@@ -35,8 +35,19 @@ class SmplNerfPipeline(NerfPipeline):
                                                                           goal_pose_encoding_flat.shape[-1])
 
         samples_encoding = self.position_encoder.encode(ray_samples)
+
         warp_field_inputs = torch.cat([samples_encoding.reshape(-1, samples_encoding.shape[-1]),
                                        goal_pose_encoding.reshape(-1, goal_pose_encoding.shape[-1])], -1)
+
+        # for warp input without encoding ################
+        goal_pose = goal_pose[..., None, :].expand(goal_pose.shape[0],
+                                                   ray_samples.shape[1],
+                                                   goal_pose.shape[-1])
+        warp_field_inputs = torch.cat(
+            [ray_samples.reshape(-1, ray_samples.shape[-1]), goal_pose.reshape(-1, goal_pose.shape[-1])], -1)
+        ############################
+
+        print(warp_field_inputs.shape)
         warp = self.model_warp_field(warp_field_inputs).view(ray_samples.shape)
         warped_samples = ray_samples + warp
         samples_encoding = self.position_encoder.encode(warped_samples)
