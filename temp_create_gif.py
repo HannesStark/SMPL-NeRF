496958a8e15ed1b9dcaa3093e41228ea8b1fe500
HannesStaerk <hannes.staerk@gmail.com>
2020-06-30 08:17:35 +0200

    adjust visualizing densities to changes in solver

--- temp_create_gif.py
+++ temp_create_gif.py
@@ -2,10 +2,10 @@ import os
 import imageio
 from tqdm import tqdm
 
-png_dir = 'renders/test'
+png_dir = 'renders/Jun23_09-32-18_korhal'
 images = []
 for file_name in tqdm(os.listdir(png_dir)):
     if file_name.endswith('.png'):
         file_path = os.path.join(png_dir, file_name)
-        images.append(imageio.imread(file_path))
-imageio.mimsave(png_dir + '/gif.gif', images,fps=50)
+        images.append(imageio.imread(file_path)[..., ::-1])
+imageio.mimsave(png_dir + '/gif.gif', images + images[::-1],fps=50)
