8f75f76c934708ffe50b484413a1538a7f8b265d
HannesStaerk <hannes.staerk@gmail.com>
2020-07-17 09:52:04 +0200

    test

--- neural_mesh_renderer.py
+++ neural_mesh_renderer.py
@@ -1,4 +1,3 @@
-import kornia
 import matplotlib
 
 matplotlib.use('Agg')
@@ -117,8 +116,7 @@ def main():
         args.camera_distance, args.elevation, azimuth)
     images, _, _ = renderer(vertices, faces, textures)
     true_image = images[0].permute(1, 2, 0)
-    if gaussian_blur:
-        true_image = kornia.filters.gaussian_blur2d(true_image.unsqueeze(0), (5, 5), sigma=(1, 1))[0]
+
     true_image = true_image.detach()
 
     if specific_angles_only and perturb_betas:
@@ -154,8 +152,7 @@ def main():
 
         images, _, _ = renderer(vertices, faces, textures)
         image = images[0].permute(1, 2, 0)
-        if gaussian_blur:
-            image = kornia.filters.gaussian_blur2d(image.unsqueeze(0), (5, 5), sigma=(1, 1))[0]
+
         loss = (image - true_image).abs().mean()
         loss.backward()
         optim.step()
