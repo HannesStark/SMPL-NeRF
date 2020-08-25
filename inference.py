5dd9f0786f1de18f01316d0724e3948826f9a312
Philipp Reiser <philipp@fam-reiser.eu>
2020-08-25 09:43:38 +0200

    change model type

--- inference.py
+++ inference.py
@@ -200,7 +200,7 @@ def config_parser_inference():
     parser.add_argument('--run_dir', default="runs/Aug24_10-50-14_korhal", help='path to load model')
     parser.add_argument('--ground_truth_dir', default="data/fix_pose_camera_-10_10_10/val",
                         help='path to load ground truth, created with create_dataset.py')
-    parser.add_argument('--model_type', default="nerf", type=str,
+    parser.add_argument('--model_type', default="append_smpl_params", type=str,
                         help='choose dataset type for model [smpl_nerf, nerf, pix2pix, smpl, append_to_nerf]')
     return parser
 
